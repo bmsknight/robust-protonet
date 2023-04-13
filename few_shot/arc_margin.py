ae2d37126ca8c11ed23ac6ec9cd190fd83a0b3a1
bmsknight <bmsknight@gmail.com>
2023-04-13 16:17:35 -0400

    Add additive angular margin code

---
+++ few_shot/arc_margin.py
@@ -0,0 +1,34 @@
+import math
+
+import torch
+
+
+
+class ArcFace(torch.nn.Module):
+    """ ArcFace (https://arxiv.org/pdf/1801.07698v1.pdf):
+    """
+    def __init__(self, s=64.0, margin=0.5):
+        super(ArcFace, self).__init__()
+        self.scale = s
+        self.margin = margin
+        self.cos_m = math.cos(margin)
+        self.sin_m = math.sin(margin)
+        self.theta = math.cos(math.pi - margin)
+        self.sinmm = math.sin(math.pi - margin) * margin
+        self.easy_margin = False
+
+    def forward(self, iplogits: torch.Tensor, labels: torch.Tensor):
+        # returning one minus because 1-cos is returned by the prev step
+        logits = 1.0 - iplogits
+        index = torch.where(labels != -1)[0]
+        target_logit = logits[index, labels[index].view(-1)]
+
+        with torch.no_grad():
+            target_logit.arccos_()
+            logits.arccos_()
+            final_target_logit = target_logit + self.margin
+            logits[index, labels[index].view(-1)] = final_target_logit
+            logits.cos_()
+        logits = logits * self.scale
+        # - cos distance is returned because loss takes negative of it
+        return -logits
