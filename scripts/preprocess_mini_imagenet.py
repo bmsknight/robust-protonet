88c1b98da37546a759ca3334a34d2a4bdb307343
Tehara-Fonseka <thanuritehara@gmail.com>
2023-06-19 21:39:47 -0400

    update script to generate dataset with a predifined miniimagenet folder split

--- scripts/preprocess_mini_imagenet.py
+++ scripts/preprocess_mini_imagenet.py
@@ -33,9 +33,18 @@ classes = list(set(classes))
 print("classes : ", classes)
 
 # Train/test split
-np.random.seed(0)
-np.random.shuffle(classes)
-background_classes, evaluation_classes = classes[:80], classes[80:]
+# np.random.seed(0)
+# np.random.shuffle(classes)
+# background_classes, evaluation_classes = classes[:80], classes[80:]
+evaluation_classes = [
+    'n01930112', 'n02091831', 'n02108551', 'n02120079', 'n02795169', \
+    'n02950826', 'n03075370', 'n04146614', 'n04389033', 'n07613480',\
+    'n02089867', 'n02105505', 'n02114548', 'n02174001', 'n02823428', \
+    'n02971356', 'n03924679', 'n04296562', 'n04509417', 'n13054560']
+
+background_classes = [c for c in classes if c not in evaluation_classes]
+
+print(len(evaluation_classes), len(background_classes))
 
 # Create class folders
 for c in background_classes:
@@ -54,4 +63,4 @@ for root, _, files in os.walk(DATA_PATH + '/miniImageNet/images'):
             subset_folder = 'images_evaluation' if class_name in evaluation_classes else 'images_background'
             src = f'{root}/{f}'
             dst = DATA_PATH + f'/miniImageNet/{subset_folder}/{class_name}/{image_name}'
-            shutil.copy(src, dst)
+            shutil.move(src, dst)
